d780f74efb80b7d2d70f721fb0898acbedbc5f12
wongbsn <wongbsn@gmail.com>
2021-02-16 13:55:33 -0500

    fix: added font

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,21 +1,24 @@
-import { createCanvas, loadImage } from "canvas";
+import { createCanvas, loadImage, registerFont } from "canvas";
 import path from "path";
 import photosData from "../../data/photos.json";
 
-async function drawImage(filePath, text = "", color = "white") {
+registerFont(path.resolve("./public", "fonts", "Cabin-SemiBold.ttf"), {
+  family: "Cabin",
+});
+
+async function drawImage(filePath, text = "", fontSize = 90, color = "white") {
   const canvas = createCanvas(1200, 600);
   const ctx = canvas.getContext("2d");
   const image = await loadImage(filePath);
-  const fontSize = 90;
-  const lineHeight = fontSize * 0.8;
+  const lineHeight = fontSize * 0.75;
 
   ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
   ctx.fillStyle = color.replace("@", "#");
   ctx.textAlign = "center";
-  ctx.font = `${fontSize}px Arial`;
+  ctx.font = `${fontSize}px Cabin`;
   ctx.shadowColor = "black";
-  ctx.shadowBlur = 7;
-  ctx.lineWidth = 5;
+  ctx.shadowBlur = 8;
+  ctx.lineWidth = 2;
 
   const lines = text.split("\\n");
   const x = 600;
@@ -55,7 +58,12 @@ export default async function (req, res) {
         path.resolve("./public", "photos", `social-${data[0].id}.jpg`)
       );
 
-      const canvas = await drawImage(filePath, req.query.text, req.query.color);
+      const canvas = await drawImage(
+        filePath,
+        req.query.text,
+        req.query.fontSize,
+        req.query.color
+      );
 
       res.setHeader("Content-Type", "image/png");
 
